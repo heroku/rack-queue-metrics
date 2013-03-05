e7cc95316e11e4720512088a2e08f8196dea15e8
Dominic Dagradi <dominic@heroku.com>
2013-03-05 15:56:09 -0800

    Post ActiveSupport::Notifications instead of logging

--- heroku-unicorn-metrics.gemspec
+++ heroku-unicorn-metrics.gemspec
@@ -16,5 +16,6 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  s.add_runtime_dependency 'unicorn'
+  s.add_dependency 'unicorn'
+  s.add_dependency 'rails', ">= 3.0"
 end
--- lib/heroku/middleware.rb
+++ lib/heroku/middleware.rb
@@ -10,31 +10,28 @@ module Heroku
       end
 
       def call(env)
-        stats = tcp_stats
-        request_start = Time.now.to_f*1000.0
+        start_time = Time.now.to_f*1000.0
+        stats = raindrops_stats
 
         status, headers, body = @app.call(env)
 
-        if headers['X-Request-Start']
-          queue_time = (request_start - headers['X-Request-Start'].to_f).round
-        else
-          queue_time = 0
-        end
+        stats[:addr] = ADDR
+        stats[:queue_time] = headers['X-Request-Start'] ? (start_time - headers['X-Request-Start'].to_f).round : 0
+        stats[:request_time] = (Time.now.to_f*1000.0 - start_time).round
 
-        request_time = (Time.now.to_f*1000.0 - request_start).round
-        puts "STATS addr=#{ADDR} conns_active=#{stats[:active]} conns_queued=#{stats[:queued]} queue_time=#{queue_time} request_time=#{request_time}"
+        ActiveSupport::Notifications.instrument("unicorn.metrics.request", stats)
 
         [status, headers, body]
       end
 
     private
 
-      def tcp_stats
+      def raindrops_stats
         if defined? Raindrops::Linux.tcp_listener_stats
           stats = Raindrops::Linux.tcp_listener_stats([ '0.0.0.0:'+ENV['PORT'] ])['0.0.0.0:'+ENV['PORT']]
-          return { :active => stats.active, :queued => stats.queued }     
+          return { :requests => { :active => stats.active, :queued => stats.queued }}
         else
-          return { :active => 0, :queued => 0 }     
+          return { :requests => { :active => 0, :queued => 0 }}
         end
       end
 
