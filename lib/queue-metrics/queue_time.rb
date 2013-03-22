fe8f3208c116b3e023a2612c76e0cc69a62b26c9
Jonathan Dance <jd@wuputah.com>
2013-03-22 11:38:19 -0700

    iterate

--- lib/queue-metrics/queue_time.rb
+++ lib/queue-metrics/queue_time.rb
@@ -14,8 +14,12 @@ module Rack
         dyno_start    = Time.now.to_f * 1000.0
         request_start = (env["HTTP_X_REQUEST_START"] || 0).to_i
         request_id    = env["HTTP_HEROKU_REQUEST_ID"]
-        notify(stats) if should_notify?
-        $stdout.puts "at=metric measure=#{@instrument_name} request_id=#{request_id} request_start=#{request_start} dyno_start=#{dyno_start} queue_time=#{dyno_start - request_start}"
+        report = "at=metric measure=#{@instrument_name} dyno_start=#{dyno_start}"
+        report << " request_start=#{request_start} request_start_delta=#{dyno_start - request_start}" if request_start > 0
+        report << " request_id=#{request_id}" if request_id
+        $stdout.puts report
+
+        notify(:dyno_start => dyno_start, :request_start => request_start, :request_id => request_id) if should_notify?
 
         @app.call(env)
       end
