5922f9c7c00635b2822c6299945758f18029c736
Chad Bailey <chadbailey@gmail.com>
2014-02-19 10:22:50 -0600

    refactor for librato, etc

--- lib/queue-metrics/railtie.rb
+++ lib/queue-metrics/railtie.rb
@@ -2,10 +2,8 @@ module Rack
   module QueueMetrics
     class RackQueueRailtie < Rails::Railtie
       initializer "rack_queue_railtie.configure_rails_initialization" do |app|
-        app.middleware.use Rack::QueueMetrics::QueueTime
         app.middleware.use Raindrops::Middleware
         app.middleware.use Rack::QueueMetrics::QueueDepth
-        app.middleware.use Rack::QueueMetrics::AppTime
       end
     end
   end
