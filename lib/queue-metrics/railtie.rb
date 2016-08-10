ee831f9b9800f8993eddcc3e976c437b5fd2cac8
Chris Lundquist <chrisl@unity3d.com>
2016-08-10 15:35:38 -0700

    [railtie] use Rails.logger by default with rails

--- lib/queue-metrics/railtie.rb
+++ lib/queue-metrics/railtie.rb
@@ -2,10 +2,10 @@ module Rack
   module QueueMetrics
     class RackQueueRailtie < Rails::Railtie
       initializer "rack_queue_railtie.configure_rails_initialization" do |app|
-        app.middleware.use Rack::QueueMetrics::QueueTime
+        app.middleware.use Rack::QueueMetrics::QueueTime, Rails.logger
         app.middleware.use Raindrops::Middleware
-        app.middleware.use Rack::QueueMetrics::QueueDepth
-        app.middleware.use Rack::QueueMetrics::AppTime
+        app.middleware.use Rack::QueueMetrics::QueueDepth, Rails.logger
+        app.middleware.use Rack::QueueMetrics::AppTime, Rails.logger
       end
     end
   end
